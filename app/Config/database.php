72bb0120f94d548270dda661c5c69f82322b0c2d
Fog <fogteen@gmail.com>
2016-04-12 14:29:21 +0700

    d1204-1429

--- app/Config/database.php
+++ app/Config/database.php
@@ -66,25 +66,31 @@
  */
 class DATABASE_CONFIG {
 
-    public $default = array(
-        'datasource' => 'Database/Mysql',
-        'persistent' => false,
-        'host' => 'localhost',
-        'login' => 'root',
-        'password' => '',
-        'database' => 'gasaon',
-        'prefix' => '',
-        'encoding' => 'utf8',
-    );
+    function __construct() {
 
-    public $test = array(
-        'datasource' => 'Database/Mysql',
-        'persistent' => false,
-        'host' => 'localhost',
-        'login' => 'user',
-        'password' => 'password',
-        'database' => 'test_database_name',
-        'prefix' => '',
-        //'encoding' => 'utf8',
-    );
+        $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+
+        $this->default = array(
+            'datasource' => 'Database/Mysql',
+            'persistent' => false,
+            'host' => $url['host'],
+            'login' => $url['user'],
+            'password' => $url['pass'],
+            'database' => substr($url['path'],1),
+            //'prefix' => '',
+            'encoding' => 'utf8',
+        );
+
+        $this->test = array(
+            'datasource' => 'Database/Mysql',
+            'persistent' => false,
+            'host' => 'localhost',
+            'login' => 'user',
+            'password' => 'password',
+            'database' => 'test_database_name',
+            'prefix' => '',
+            //'encoding' => 'utf8',
+        );
+
+    }
 }
