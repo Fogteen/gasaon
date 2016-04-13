99a3d0197bf7f0a37ced17fa27f6d68be1559fe5
Fog <fogteen@gmail.com>
2016-04-13 08:38:57 +0700

    d1304-0838

--- app/Config/database.php
+++ app/Config/database.php
@@ -67,31 +67,31 @@
 class DATABASE_CONFIG
 {
 
-    public $default;
+    function __construct() {
 
-    public $test = array(
-        'datasource' => 'Database/Mysql',
-        'persistent' => false,
-        'host' => 'localhost',
-        'login' => 'user',
-        'password' => 'password',
-        'database' => 'test_database_name',
-        'prefix' => '',
-        //'encoding' => 'utf8',
-    );
+        $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
 
-    public function __construct()
-    {
-        $DATABASE_URL = parse_url($_ENV['DATABASE_URL']);
         $this->default = array(
             'datasource' => 'Database/Mysql',
             'persistent' => false,
-            'host' => $DATABASE_URL['host'],
-            'login' => $DATABASE_URL['user'],
-            'password' => $DATABASE_URL['pass'],
-            'database' => substr($DATABASE_URL['path'], 1),
+            'host' => $url['host'],
+            'login' => $url['user'],
+            'password' => $url['pass'],
+            'database' => substr($url['path'],1),
             'prefix' => '',
             'encoding' => 'utf8',
         );
+
+        $this->test = array(
+            'datasource' => 'Database/Mysql',
+            'persistent' => false,
+            'host' => 'localhost',
+            'login' => 'user',
+            'password' => 'password',
+            'database' => 'test_database_name',
+            'prefix' => '',
+            //'encoding' => 'utf8',
+        );
+
     }
 }
